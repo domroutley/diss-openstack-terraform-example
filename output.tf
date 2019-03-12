55da1ec62f916278f3da95aaa480e6eea230c470
domroutley <dominic.routley@gmail.com>
2019-03-12 14:10:31 +0000

    added comments to output file

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "IP_Address" {
-  value = "${openstack_networking_floatingip_v2.floating_ip.address}"
+  value = "${openstack_networking_floatingip_v2.floating_ip.address}"           # This outputs the IP address of the server at the end of the Terraform run and when you run "Terraform output"
 }
